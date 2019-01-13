4a30d56149616475feef60a5af9cc6deb32555b7
Iago Caldeira <iagocaldeiraa@gmail.com>
2019-01-13 19:59:04 -0200

    features

--- layouts/Features/Features.js
+++ layouts/Features/Features.js
@@ -1,23 +1,35 @@
 export default () => (
-  <div>
-    <h2>
-      We envision a world where everyone has permanent access to a hiking trail
-    </h2>
-    <div>
-      We’ve Mobilized
-      <div>NUMBER</div>
-      Trail Volunteers.
+    <div className="features">
+        <h2 className="features--title">
+            We envision a world where everyone has permanent access to a hiking trail
+        </h2>
+        <div
+            className="features--card"
+            id="features--advocate"
+        >
+            <h3 className="features--card--title">Advocate</h3>
+            We work with Congress, Federal agencies, and partners on policy
+            issues and legislation to ensure funding for trails, preservation of
+            natural areas, and protection of the hiking experience.
+        </div>
+        <div
+            className="features--card"
+            id="features--volunteer"
+        >
+            <h3 className="features--card--title">Volunteer</h3>
+            Trails don't maintain themselves. Individuals of any age and
+            experience level can become trail stewards. Give back to the trails you
+            love, meet new people, and enjoy the experience!
+        </div>
+        <div
+            className="features--card"
+            id="features--explore"
+        >
+            <h3 className="features--card--title">Explore</h3>
+            Feel confident when you set out on your next trail adventure.
+            Check out our free hiking resources like tips for hiking with families,
+            gear checklists, finding the best trail for you.
+        </div>
+        <a className="features--learnMore">Learn More</a>
     </div>
-    <div>
-      We’ve Facilitated
-      <div>NUMBER</div>
-      Miles of Trail Maintenance & Construction .
-    </div>
-    <div>
-      Volunteers Have Contributed
-      <div>NUMBER</div>
-      in Volunteer Labor .
-    </div>
-    <div>Learn More</div>
-  </div>
 );
