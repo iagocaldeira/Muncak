6956ec88338e54a77c54a7af32a05df14317f874
Iago Caldeira <iagocaldeiraa@gmail.com>
2019-01-13 19:58:50 -0200

    Contadores

--- layouts/Counters/Counters.js
+++ layouts/Counters/Counters.js
@@ -1,24 +1,22 @@
 export default () => (
-  <div>
-    <h2>
-      With the support of our members, and partners, we've improved trail access
-      for millions of people
-    </h2>
-    <div>
-      Advocate We work with Congress, Federal agencies, and partners on policy
-      issues and legislation to ensure funding for trails, preservation of
-      natural areas, and protection of the hiking experience.
+    <div className="counters">
+        <h2 className="counters--title">
+            With the support of our members, and partners, we've improved trail access for millions of people
+        </h2>
+        <div className="counters--card">
+            We’ve Mobilized
+            <div className="counters--card--numbers">558,768</div>
+            Trail Volunteers.
+        </div>
+        <div className="counters--card">
+            We’ve Facilitated
+            <div className="counters--card--numbers">41,146</div>
+            Miles of Trail Maintenance & Construction.
+        </div>
+        <div className="counters--card">
+            Volunteers Have Contributed
+            <div className="counters--card--numbers">$108 Million</div>
+            in Volunteer Labor.
+        </div>
     </div>
-    <div>
-      Volunteer Trails don't maintain themselves. Individuals of any age and
-      experience level can become trail stewards. Give back to the trails you
-      love, meet new people, and enjoy the experience!
-    </div>
-    <div>
-      Explore Feel confident when you set out on your next trail adventure.
-      Check out our free hiking resources like tips for hiking with families,
-      gear checklists, finding the best trail for you.
-    </div>
-    <div>Learn More</div>
-  </div>
 );
