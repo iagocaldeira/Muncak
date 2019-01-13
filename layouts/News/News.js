f40c9b46bad0b2880bbb44e5cf4616d9500a62ea
Iago Caldeira <iagocaldeiraa@gmail.com>
2019-01-13 19:59:25 -0200

    news

--- layouts/News/News.js
+++ layouts/News/News.js
@@ -1,33 +1,36 @@
 export default () => (
-  <div>
-    <h2>Protecting Trails: Current News and Issues </h2>
-    <div>
-      <div>2,802 Miles of Trail to be Improved on International Trails Day</div>
-      <div>
-        On June 2, 2018, people across the worldwill come together to
-        collectively improve 2,802 miles of trail—the distance across the
-        world—during International Trails Day. Muncak invites hikers, cyclists,…
-      </div>
-      <div>Read More</div>
+    <div className="news">
+        <h2 className="news--title">Protecting Trails: Current News and Issues </h2>
+        <div className="news--card">
+            <img className="news--card--image" src={require('../../static/images/news1.webp')} />
+            <div className="news--card--title">2,802 Miles of Trail to be Improved on International Trails Day</div>
+            <div className="news--card--text">
+                On June 2, 2018, people across the worldwill come together to
+                collectively improve 2,802 miles of trail—the distance across the
+                world—during International Trails Day. Muncak invites hikers, cyclists,…
+            </div>
+            <div className="news--card--readMore">Read More</div>
+        </div>
+        <div className="news--card">
+            <img className="news--card--image" src={require('../../static/images/news2.webp')} />
+            <div className="news--card--title">Four Ways to Celebrate International Trails Day on a Bicycle</div>
+            <div className="news--card--text">
+                There are many ways to celebrate the International Trails Day on June 2.
+                It’s even possible to participate on a bicycle. And if you go on a bike
+                overnight trip during International Trails Dayyou will simultaneously…
+            </div>
+            <div className="news--card--readMore">Read More</div>
+        </div>
+        <div className="news--card">
+            <img className="news--card--image" src={require('../../static/images/news3.webp')} />
+            <div className="news--card--title"> Millennials are the Future of our Trails</div>
+            <div className="news--card--text">
+                The smell of fresh dew is a welcome scent, as the crunch of my boots
+                melds with the sound of the birds singing their morning song. It’s early
+                and I am wearing layers, but it won’t be long until they are peeled away
+                to adapt to the summer…
+            </div>
+            <div className="news--card--readMore">Read More</div>
+        </div>
     </div>
-    <div>
-      <div>Four Ways to Celebrate International Trails Day on a Bicycle</div>
-      <div>
-        There are many ways to celebrate the International Trails Day on June 2.
-        It’s even possible to participate on a bicycle. And if you go on a bike
-        overnight trip during International Trails Dayyou will simultaneously…
-      </div>
-      <div>Read More</div>
-    </div>
-    <div>
-      <div> Millennials are the Future of our Trails</div>
-      <div>
-        The smell of fresh dew is a welcome scent, as the crunch of my boots
-        melds with the sound of the birds singing their morning song. It’s early
-        and I am wearing layers, but it won’t be long until they are peeled away
-        to adapt to the summer…
-      </div>
-      <div>Read More</div>
-    </div>
-  </div>
 );
