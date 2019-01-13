b7d808772c91a357197bbd240ef6c46866b5b4c4
Iago Caldeira <iagocaldeiraa@gmail.com>
2019-01-13 19:59:47 -0200

    adicionando novos componentes ao index

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,19 @@
 import "../styles/Main.scss";
 import Hero from "../layouts/Hero/Hero";
 import Header from "../layouts/Header/Header";
+import Features from "../layouts/Features/Features";
+import Counters from "../layouts/Counters/Counters";
+import News from "../layouts/News/News";
+import Form from "../layouts/Form/Form";
 
 
 export default () => (
-  <div>
-    <Header />
-    <Hero />
-  </div>
+    <div>
+        <Header />
+        <Hero />
+        <Features />
+        <Counters/>
+        <News/>
+        <Form/>
+    </div>
 )
