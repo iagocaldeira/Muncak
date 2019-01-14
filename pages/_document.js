5edb0e7c5e6dd1c6907decb773b9e17a5a3c2db5
Iago Caldeira <iagocaldeiraa@gmail.com>
2019-01-14 17:56:01 -0200

    informações do opengraph

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,14 @@ export default class MyDocument extends Document {
                     <meta name="description" content="Muncak" />
                     <meta name="msapplication-TileColor" content="#2870DB" />
                     <meta name="theme-color" content="#2870DB" />
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+                    <meta property="og:title" content="Muncak" />
+                    <meta property="og:site_name" content="Muncak" />
+                    <meta property="og:url" content="http://muncak.com" />
+                    <meta property="og:description" content="Here is the hiking information you need to decide if it is right for you, and to make it more interesting, enjoyable, and safe." />
+                    <meta property="og:type" content="website" />
+                    <meta property="og:image" content="http://muncak.com/static/images/hero_illustration.webp" />
                 </Head>
                 <body>
                     <Main />
